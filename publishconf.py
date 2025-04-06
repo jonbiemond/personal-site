f4dcd1e6f02981b0f3ce22b19a94604ed56b44bc
Jonathan Biemond <jonbiemond@gmail.com>
2025-04-06 08:24:40 +0200

    cleanup config

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://jonathanbiemond.com"
 RELATIVE_URLS = False
 
@@ -15,8 +14,3 @@ FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = ""
